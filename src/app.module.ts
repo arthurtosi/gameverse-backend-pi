f3eaf5c4e5617fd73809050d3ddea620f34d1ab9
PedroSodreUFES <pedro.malini@edu.ufes.br>
2025-06-02 19:19:21 -0300

    feat: updating database + making authentication and create account

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,6 @@ import { ConfigModule } from "@nestjs/config";
 import { envSchema } from "./env";
 import { AuthModule } from "./auth/auth.module";
 import { AuthenticateController } from "./controllers/authenticate.controller";
-import { CreatePostController } from "./controllers/create-post.controller";
-import { FetchUserPostsController } from "./controllers/fetch-user-posts.controller";
-import { FetchPostsController } from "./controllers/fetch-all-posts.controller";
-import { EditProfilePhotoController } from "./controllers/edit-profile-photo.controller";
-import { DeletePostController } from "./controllers/delete-post.controller";
-import { EditPostDescriptionController } from "./controllers/edit-post-description.controller";
-import { FetchUsersController } from "./controllers/fetch-users.controller";
 
 @Module({
   imports: [
@@ -21,17 +14,7 @@ import { FetchUsersController } from "./controllers/fetch-users.controller";
     }),
     AuthModule,
   ],
-  controllers: [
-    CreateAccountController,
-    AuthenticateController,
-    CreatePostController,
-    FetchUserPostsController,
-    FetchPostsController,
-    EditProfilePhotoController,
-    DeletePostController,
-    EditPostDescriptionController,
-    FetchUsersController,
-  ],
+  controllers: [AuthenticateController, CreateAccountController],
   providers: [PrismaService],
 })
 export class AppModule {}
